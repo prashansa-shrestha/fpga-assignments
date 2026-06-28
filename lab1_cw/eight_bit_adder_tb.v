22600bdf2a50c73c81a16d4a9213fe01d88efacf
Prashansa Shrestha <prashansashrestha1@gmail.com>
2026-06-28 21:17:10 +0545

    lab 1 tasks

---
+++ lab1_cw/eight_bit_adder_tb.v
@@ -0,0 +1,29 @@
+module eight_bit_adder_tb;
+
+reg [7:0] a,b;
+reg c_in;
+
+wire [7:0] sum;
+wire c_out;
+
+fulladd8 sachin(
+    .a(a), .b(b),
+    .c_in(c_in),
+    .sum(sum),
+    .c_out(c_out)
+);
+
+initial begin
+
+    $dumpfile("eight_bit_adder.vcd");
+    $dumpvars(0,sachin);
+
+    a=8'h5f;
+    b=8'ha2;
+
+    c_in=1;
+
+    #100;
+
+end
+endmodule
